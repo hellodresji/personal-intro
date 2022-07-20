820e705dbbe5ddc4c9ba62378033ffdb4f468066
季光辉 <236772218@qq.com>
2022-07-20 19:49:43 +0200

    update deploy

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
 # cv-template
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Project development
+
+After commit the built project to gh-pages branch, the pages are available via: https://hellodresji.github.io/personal-intro/profile/index.html#/
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,33 @@
-import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
-import Resume from '@/views/Resume.vue';
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Home from "../views/Home.vue";
+import Resume from "@/views/Resume.vue";
 
 Vue.use(VueRouter);
 
 const routes: RouteConfig[] = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
-    path: '/cv',
+    path: "/cv",
     component: Resume,
   },
 ];
 
 const router = new VueRouter({
-  // mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes,
 });
