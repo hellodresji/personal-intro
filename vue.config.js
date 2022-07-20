3a2da2910fbd40111e59ddd65c200cdfa80b05ea
季光辉 <236772218@qq.com>
2022-07-20 20:14:26 +0200

    update deploy

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
+const path = require("path");
+
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  publicPath: process.env.NODE_ENV === 'development'
-  ? ''
-  : './',
-  outputDir: './profile',
-}
+  transpileDependencies: ["vuetify"],
+  publicPath:
+    process.env.NODE_ENV === "development" ? "" : "/personal-intro/profile",
+  outputDir: "./profile",
+};
